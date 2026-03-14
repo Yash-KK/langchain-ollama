7e5e6ccabb6af2e94ae21f56ad58f9b12a9bc45d
Yash-KK <reachyashk@gmail.com>
2026-03-14 13:23:28 +0530

    using togetherai

--- helpers/common.py
+++ helpers/common.py
@@ -1,6 +1,8 @@
+import os
 from dotenv import load_dotenv
 from langfuse import get_client
 from langfuse.langchain import CallbackHandler
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -11,5 +13,14 @@ NOMIC_EMBEDDING_MODEL = "nomic-embed-text:latest"
 
 BASE_URL = "http://localhost:11434/"
 
+TOGETHER_API_KEY= os.getenv('TOGETHER_API_KEY') 
 langfuse = get_client()
 langfuse_handler = CallbackHandler()
+
+
+together_ai_llm = ChatOpenAI(
+  api_key=TOGETHER_API_KEY,
+  model="OpenAI/gpt-oss-20B",
+  base_url="https://api.together.xyz/v1",
+  callbacks=[langfuse_handler]
+)
