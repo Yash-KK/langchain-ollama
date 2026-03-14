b4999c2d3bc32a8d1bbc72c1337805bd4401450f
Yash-KK <reachyashk@gmail.com>
2026-03-14 12:17:58 +0530

    sequential chain

--- helpers/common.py
+++ helpers/common.py
@@ -1,6 +1,15 @@
-GEMMA_MODEL="gemma3:4b"                      
-DEEPSEEK_MODEL="deepseek-r1:1.5b"               
-QWEN_MODEL="qwen2.5:7b"                     
-NOMIC_EMBEDDING_MODEL="nomic-embed-text:latest"    
+from dotenv import load_dotenv
+from langfuse import get_client
+from langfuse.langchain import CallbackHandler
 
-BASE_URL="http://localhost:11434/"
+load_dotenv()
+
+GEMMA_MODEL = "gemma3:4b"
+DEEPSEEK_MODEL = "deepseek-r1:1.5b"
+QWEN_MODEL = "qwen2.5:7b"
+NOMIC_EMBEDDING_MODEL = "nomic-embed-text:latest"
+
+BASE_URL = "http://localhost:11434/"
+
+langfuse = get_client()
+langfuse_handler = CallbackHandler()
